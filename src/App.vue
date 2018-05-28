270e1212fe111f31caa7508040c5e06d20aa380a
Evan Sparkman <me@evansparkman.com>
2018-05-28 18:25:12 -0400

    API Calls are working

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,26 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <app-header/>
     </div>
     <router-view/>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
+<script>
+import AppHeader from '@/app-header'
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+export default {
+  name: 'App',
+  components: {
+    'app-header': AppHeader
+  },
+  created() {
+    this.$store.dispatch('getCoins')
+    this.$store.dispatch('getMarketData')
+  }
 }
+</script>
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+<style src="@/assets/styles/app.css">
 </style>
---
+++ src/app-header.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="ui top attached menu">
+    <div class="item name">
+      SimpleCrypto
+    </div>
+    <div class="ui icon item">
+      <i class="home icon"></i>
+    </div>
+    <div class="ui icon item">
+      <i class="question icon"></i>
+    </div>
+    <div class="right menu">
+      <div class="ui right aligned category search item">
+        <div class="ui transparent icon input">
+          <v-select
+            id="v-select"
+            placeholder="Search..."
+            :options="coins"
+            label="name"
+          ></v-select>
+          <i class="search link icon"></i>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'AppHeader',
+  computed: {
+    ...mapGetters(['coins'])
+  }
+}
+</script>
+
+<style src="@/assets/styles/app-header.css">
+</style>
+
